b34daab487c5a127a6b2a5e9f4d2842febe4dc6c
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-04-17 17:46:49 -0500

    Hello World!

---
+++ src/main/java/com/accenture/accchallenge/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.accenture.accchallenge;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class HelloWorldController {
+
+    @GetMapping
+    public String saludar(){
+        return "Hello World! Never stop learning! 🚀";
+    }
+}
