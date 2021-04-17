694b4a49ea600096f6a2f6e40a89ec01cf027cd5
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-04-17 17:32:37 -0500

    Initializr

---
+++ src/main/java/com/accenture/accchallenge/AccChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.accchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/accchallenge/AccChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.accchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
